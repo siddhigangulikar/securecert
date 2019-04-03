a0914d2e49e779b6d0216719b5c75e028d588004
siddhigangulikar <siddhigangulikar@gmail.com>
2019-04-03 17:24:24 +0530

    top-nav complete

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TopnavComponent } from './topnav/topnav.component';
+import { MatIconModule, MatToolbarModule, MatMenuModule, MatListModule } from '@angular/material';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children:[
+      {
+        path:'',
+        loadChildren: './home/home.module#HomeModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [MainComponent, TopnavComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatListModule,
+
+
+  ]
+})
+export class MainModule { }
